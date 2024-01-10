1d0eeeaa2a375720b199f4ef252a3711451b1a1c
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2024-01-10 13:26:47 -0500

    Initial Commit

---
+++ AddingMachine/AddingMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddingMachineApp.swift
+//  AddingMachine
+//
+//  Created by Lewis Brocklehurst on 2024-01-10.
+//
+
+import SwiftUI
+
+@main
+struct AddingMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
