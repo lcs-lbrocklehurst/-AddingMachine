d08bc836e486ef347cbfe7218de7d60a31fa803a
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2024-01-10 13:38:26 -0500

    Organized the project files and started to make main inerface

---
+++ AddingMachine/Views/QuizView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizView.swift
+//  AddingMachine
+//
+//  Created by Lewis Brocklehurst on 2024-01-10.
+//
+
+import SwiftUI
+
+struct QuizView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizView()
+}
